f8cf7f3a5f06a6f0d99857a624aded757dfe6a21
Fallenwood <yuy0216@mail.ustc.edu.cn>
2018-11-09 14:19:48 +0800

    Removed u''

--- doc/conf.py
+++ doc/conf.py
@@ -48,8 +48,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'NGreenletProfiler'
-copyright = u'2014, A. Jesse Jiryu Davis'
+project = 'NGreenletProfiler'
+copyright = '2014, A. Jesse Jiryu Davis'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -201,8 +201,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'NGreenletProfiler.tex', u'NGreenletProfiler Documentation',
-   u'A. Jesse Jiryu Davis', 'manual'),
+  ('index', 'NGreenletProfiler.tex', 'NGreenletProfiler Documentation',
+   'A. Jesse Jiryu Davis', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -231,8 +231,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'ngreenletprofiler', u'NGreenletProfiler Documentation',
-     [u'fallenwood'], 1)
+    ('index', 'ngreenletprofiler', 'NGreenletProfiler Documentation',
+     ['fallenwood'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -245,8 +245,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'NGreenletProfiler', u'NGreenletProfiler Documentation',
-   u'fallenwood', 'NGreenletProfiler', 'One line description of project.',
+  ('index', 'NGreenletProfiler', 'NGreenletProfiler Documentation',
+   'fallenwood', 'NGreenletProfiler', 'One line description of project.',
    'Miscellaneous'),
 ]
 
